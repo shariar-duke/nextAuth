2394636d27a91214fa851004c1b4c4f586c7cc39
shariar-duke <shariarmahmud225@gmail.com>
2024-06-06 19:47:36 +0600

    showing iamges and fixing next config files

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,13 +1,27 @@
-
-import {auth, signIn , signOut} from "@/auth"
-import Image from "next/image"
-import Link from "next/link"
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 export default async function Header() {
   const session = await auth();
-  console.log("The session data is", session)
+  console.log("The session data is", session);
   return (
     <div>
-      
+      {session?.user ? (
+        <div>
+          {<p>{session?.user?.name}</p>} |
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </div>
+      ) : (
+        <div>
+          <p>Ask to Login</p>{" "}
+        </div>
+      )}
     </div>
-  )
+  );
 }
