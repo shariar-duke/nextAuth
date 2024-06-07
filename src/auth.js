3c369c0cce45761a7017e149def3fb5fd2d8c256
shariar-duke <shariarmahmud225@gmail.com>
2024-06-07 18:18:18 +0600

    sucessfully logged in with gamail and kept informations in mongodb

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"; // eta k import kora holo
+import mongoClientPromise from "../lib/mongoClientPromise"; // eat k import kora holo 
 
 export const {
   // egula jno api leve eo handle korte pare...
@@ -8,6 +10,7 @@ export const {
   signIn, 
   signOut
 } = NextAuth({
+    adapter: MongoDBAdapter(mongoClientPromise, {databaseName: process.env.ENVIRONMENT}),
     providers:[ GoogleProvider({
         clientId : process.env.GOOGLE_CLIENT_ID,
         clientSecret:process.env.GOOGLE_CLIENT_SECRET
@@ -15,13 +18,4 @@ export const {
    
 })
 
-// ekhnae amra nextAuth thek kisu jinis timport kore nische niye export kore disch. 
-//export kore diche holo auth, signIn, signOut, 
 
-// NextAuth perameter hisebe ekta object nibe . Ei object er ekta peramter holo providers 
-// ekhn onk kisu diayai next auth er help nia login kora jay jmn Google, Github, etc. Facebook . Tai providers ta holo ekta array
-// Er modhe amra apatoto GoogleProviders k rakhse 
-// eta peratmeter hisbe ekta object ney
-// ei object er apatoto duta property 
-// clientId => ekta client Id  
-// clientSecret => ekta secret mesage localhost 300 theke google er kase jabe 
